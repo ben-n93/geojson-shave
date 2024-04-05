c8bbe42fe0bc3d9e3aac17ea8850049b51ce7ab8
Benjamin Nour <ben.nour@live.com.au>
2024-04-05 17:13:24 +1100

    updated versioning

--- geojson_shave/__init__.py
+++ geojson_shave/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for geojson-shave."""
 
 __author__ = """Ben Nour"""
-__email__ = 'hello@ben-nour.com'
-__version__ = '0.1.1'
+__email__ = "hello@ben-nour.com"
+__version__ = "0.1.2"
